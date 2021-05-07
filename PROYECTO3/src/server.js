5f75c142c73606992989a4f9a7708689051b0e95
luisgarces1 <72531010+luisgarces1@users.noreply.github.com>
2021-05-07 12:32:45 -0500

    cambio 1 7/5/20201

---
+++ PROYECTO3/src/server.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+
+
+
+const app =  express ();
+
+const port = 3000;
+
+server.listen(port, () =>{
+    console.log(`servidor corriendo en el puerto ${port}`);
+});
