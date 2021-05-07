505ef40520be5910586858657d6b05ba1e7f6b44
luisgarces1 <72531010+luisgarces1@users.noreply.github.com>
2021-05-07 14:54:49 -0500

    cambio de el proyecto para acamica

--- PROYECTO3/src/server.js
+++ PROYECTO3/src/server.js
@@ -1,11 +1,13 @@
 const express = require ('express');
 
-
-
 const app =  express ();
 
-const port = 3000;
+const port = 3004;
 
-server.listen(port, () =>{
+app.get('/', (req, res) => {
+    res.send('Bienvenidos a mi nueva api de express');
+});
+
+app.listen(port, () =>{
     console.log(`servidor corriendo en el puerto ${port}`);
 });
